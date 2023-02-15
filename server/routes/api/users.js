f2471e166db121e29a3125a3c023469f5b0a5f55
Cady Baltz <cadybaltz@gmail.com>
2023-02-15 12:34:19 -0600

    Finished User API endpoints

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,40 +1,57 @@
 // routes/api/users.js
+// Defines API endpoints for creating, updating, and deleting users from the database
 
 const express = require('express');
-//const router = express.Router();
 var app = express();
 app.use(express.json());
 
-// Load User model
 const User = require('../../models/User');
 
 // @route GET api/users
 // @description Get all users
 // @access Public
-app.get('/', (req, res) => {
-  console.log(req.body)
+app.get('/', (_, res) => {
   User.find()
     .then(users => res.json(users))
-    .catch(err => res.status(404).json({ nousersfound: 'No Users found' }));
+    .catch(err => res.status(404).json({ nousersfound: err }));
 });
 
-// @route GET api/users/:email
-// @description Get single user by email
+// @route GET api/users/:id
+// @description Get single user by id
 // @access Public
-app.get('/:email', (req, res) => {
-  User.findById(req.params.email)
+app.get('/:id', (req, res) => {
+  User.findById(req.params.id)
     .then(user => res.json(user))
-    .catch(err => res.status(404).json({ nouserfound: 'No User found' }));
+    .catch(err => res.status(404).json({ nouserfound: err }));
 });
 
 // @route POST api/users
-// @description add/save user
+// @description Create a new user
 // @access Public
 app.post('/', (req, res) => {
-  console.log(req.body)
   User.create(req.body)
-    .then(user => res.json({ msg: 'User added successfully' }))
-    .catch(err => res.status(400).json({ error: 'Unable to add this user' }));
+    .then(user => res.json({ msg: 'User ' + user.id + ' added successfully' }))
+    .catch(err => res.status(400).json({ error: err }));
+});
+
+// @route PUT api/users/:id
+// @description Update user
+// @access Public
+app.put('/:id', (req, res) => {
+  User.findByIdAndUpdate(req.params.id, req.body)
+    .then(user => res.json({ msg: 'Updated user ' + user.id + ' successfully' }))
+    .catch(err =>
+      res.status(400).json({ error: err })
+    );
+});
+
+// @route DELETE api/users/:id
+// @description Delete user by id
+// @access Public
+app.delete('/:id', (req, res) => {
+  User.findByIdAndRemove(req.params.id, req.body)
+    .then(user => res.json({ mgs: 'User ' + user.id + ' deleted successfully' }))
+    .catch(err => res.status(404).json({ error: err }));
 });
 
 module.exports = app;
