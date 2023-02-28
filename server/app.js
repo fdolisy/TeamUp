e117d40330b64b69538892ed1e3209ad6e804c1a
Swarita <swarita.kelwada@gmail.com>
2023-02-28 17:23:40 -0600

    pulling from master

--- server/app.js
+++ server/app.js
@@ -1,9 +1,26 @@
 // app.js
 
+
 const express = require('express');
 const connectDB = require('./config/db');
 
+
+
 const app = express();
+
+
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+
+
+
 
 const team_routes = require('./routes/api/teams');
 const login_route = require('./routes/api/login');
@@ -14,8 +31,11 @@ app.use('/api/register', registration_route)
 app.use('/api/login', login_route)
 //app.use('/api/teams/:id/submit', submit_team_route)
 
+
 // Connect Database
 connectDB();
+
+app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 8082;
 
---
+++ server/config.js
@@ -0,0 +1,2 @@
+ATLAS_URI=mongodb+srv://Cluster13473:teamup@cluster13473.ziewtw1.mongodb.net/test?retryWrites=true&w=majority
+PORT=5000
