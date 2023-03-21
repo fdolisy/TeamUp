1550034189b3b5971894b9b7afc8e3d9d325c413
sidkumar5 <sidharth.usa89@gmail.com>
2023-03-21 13:22:13 -0500

    created table for JoinTeam page with data from backend

--- client/src/pages/JoinTeam.js
+++ client/src/pages/JoinTeam.js
@@ -1,41 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import axios from "axios";
 
-const project_names = [];
-const objs = [];
-const projectOptions = [];
-
-axios
-  .get("http://localhost:8082/api/projects/")
-  .then((response) => {
-    const project_objects = new Array(response.data);
-
-    for (var i = 0; i < project_objects[0].length; i++) {
-      // array of project objects
-      objs.push(project_objects[0][i]);
-    }
-    objs.forEach((object) => {
-      // array of project names
-      project_names.push(object["name"]);
-      projectOptions.push({
-        name: object["name"],
-        number: object["number"],
-        desired_skills: object["desired_skills"],
-        sponsor: object["sponsor"],
-      });
-    });
-
-    console.log(projectOptions);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
 export default function JoinTeam() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8082/api/projects/")
+      .then((response) => {
+        // const project_objects = new Array(response.data);
+        setProjects(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  function handleClick(project) {
+    console.log(`Clicked on project "${project.name}"`);
+  }
+
   return (
     <div className="bg-offWhite h-screen">
       <Navbar />
+
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Name
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Number
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Desired Skills
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              Sponsor
+            </th>
+          </tr>
+        </thead>
+
+        <tbody className="bg-offWhite divide-y divide-gray-200">
+          {projects.map((project) => (
+            <tr
+              className="hover:bg-orange cursor-pointer"
+              key={project.id}
+              onClick={() => handleClick(project)}
+            >
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm font-medium text-gray-900">
+                  {project.name}
+                </div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm text-gray-500">{project.number}</div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm text-gray-500">{project.sponsor}</div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                {project.desired_skills.join(", ")}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
