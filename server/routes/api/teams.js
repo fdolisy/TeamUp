3c677f2be5896053f75230821ed0b6788a502e93
cadybaltz <cadybaltz@gmail.com>
2023-03-21 19:33:15 -0500

    Stopped updating user team if the team is private

--- server/routes/api/teams.js
+++ server/routes/api/teams.js
@@ -139,60 +139,60 @@ app.put('/join/:id', auth, (req, res) => {
           if (!team.members.includes(req.body.user_id)) {
             team.members.push(req.body.user_id)
           }
+
+          // now, update the user to contain the team ID they are a part of
+          User.findById(req.body.user_id)
+            .then(user => {
+
+              // if the user is already on a team, remove them from that team
+              if (user.team && user.team != team.id) {
+                Team.findById(user.team)
+                  .then(old_team => {
+
+                    // if the user is on a finalized team already, do not allow them to join a new team
+                    if (old_team.is_finalized) {
+                      res.status(400).json({ mgs: 'User ' + user.id + ' is already on a finalized team and cannot join a new one' })
+                    } else {
+
+                      // remove user from their old team
+                      team_without_user = []
+                      for (let i = 0; i < old_team.members.length; i++) {
+                        if (old_team.members[i] != req.body.user_id) {
+                          team_without_user.push(old_team.members[i])
+                        }
+                      }
+
+                      old_team.members = team_without_user
+
+                      // if this user was the only member on that team, delete the old team completely
+                      if (team_without_user.length == 0) {
+                        old_team.delete()
+                          .then(old_team => console.log("Deleted team " + old_team.id))
+                      } else {
+                        old_team.save()
+                          .then(old_team => console.log("Removed user " + req.body.user_id + " from team " + old_team.id))
+                      }
+
+                      // add user to their new team
+                      user.team = team.id
+                      user.save()
+                        .then(user => console.log("Updated user " + user.id + " to store team ID " + team.id))
+                      team.save();
+                      res.json({ mgs: 'User ' + req.body.user_id + ' successfully joined team ' + team.id })
+                    }
+                  })
+              } else {
+                // otherwise, always add the user to the new team
+                user.team = team.id
+                team.save();
+                user.save()
+                  .then(user => console.log("Updated user " + user.id + " to store team ID " + team.id))
+                res.json({ mgs: 'User ' + req.body.user_id + ' successfully joined team ' + team.id })
+              }
+            })
         } else {
           console.log("TODO: implement logic for private teams - see issue #17 in GitHub")
         }
-
-        // now, update the user to contain the team ID they are a part of
-        User.findById(req.body.user_id)
-          .then(user => {
-
-            // if the user is already on a team, remove them from that team
-            if (user.team && user.team != team.id) {
-              Team.findById(user.team)
-                .then(old_team => {
-
-                  // if the user is on a finalized team already, do not allow them to join a new team
-                  if (old_team.is_finalized) {
-                    res.status(400).json({ mgs: 'User ' + user.id + ' is already on a finalized team and cannot join a new one' })
-                  } else {
-
-                    // remove user from their old team
-                    team_without_user = []
-                    for (let i = 0; i < old_team.members.length; i++) {
-                      if (old_team.members[i] != req.body.user_id) {
-                        team_without_user.push(old_team.members[i])
-                      }
-                    }
-
-                    old_team.members = team_without_user
-
-                    // if this user was the only member on that team, delete the old team completely
-                    if (team_without_user.length == 0) {
-                      old_team.delete()
-                        .then(old_team => console.log("Deleted team " + old_team.id))
-                    } else {
-                      old_team.save()
-                        .then(old_team => console.log("Removed user " + req.body.user_id + " from team " + old_team.id))
-                    }
-
-                    // add user to their new team
-                    user.team = team.id
-                    user.save()
-                      .then(user => console.log("Updated user " + user.id + " to store team ID " + team.id))
-                    team.save();
-                    res.json({ mgs: 'User ' + req.body.user_id + ' successfully joined team ' + team.id })
-                  }
-                })
-            } else {
-              // otherwise, always add the user to the new team
-              user.team = team.id
-              team.save();
-              user.save()
-                .then(user => console.log("Updated user " + user.id + " to store team ID " + team.id))
-              res.json({ mgs: 'User ' + req.body.user_id + ' successfully joined team ' + team.id })
-            }
-          })
       }
     })
     .catch(err => res.status(400).json({ error: err }));
